fcb4d57c40d799883912e70c154d9159047575d2
ARYANatGIT <anonymous.guy.26072006@gmail.com>
2025-04-25 02:45:53 +0530

    Update Q1.c

--- DS-Lab-Evaluation (III)/Q1.c
+++ DS-Lab-Evaluation (III)/Q1.c
@@ -8,41 +8,41 @@ struct Node {
 };
 
 struct Node* newNode(int data) {
-    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
-    node->data = data;
-    node->left = node->right = NULL;
+    struct Node* node=(struct Node*)malloc(sizeof(struct Node));
+    node->data=data;
+    node->left=node->right=NULL;
     return node;
 }
 
 int search(int inorder[], int start, int end, int value) {
-    for (int i = start; i <= end; i++) {
-        if (inorder[i] == value)
+    for (int i=start;i<=end;i++) {
+        if (inorder[i]==value)
             return i;
     }
     return -1;
 }
 
 struct Node* buildTree(int inorder[], int preorder[], int inStart, int inEnd, int* preIndex) {
-    if (inStart > inEnd)
+    if (inStart>inEnd)
         return NULL;
 
-    int current = preorder[*preIndex];
+    int current=preorder[*preIndex];
     (*preIndex)++;
-    struct Node* node = newNode(current);
+    struct Node* node=newNode(current);
 
-    if (inStart == inEnd)
+    if (inStart==inEnd)
         return node;
 
-    int inIndex = search(inorder, inStart, inEnd, current);
+    int inIndex=search(inorder, inStart, inEnd, current);
 
-    node->left = buildTree(inorder, preorder, inStart, inIndex - 1, preIndex);
-    node->right = buildTree(inorder, preorder, inIndex + 1, inEnd, preIndex);
+    node->left=buildTree(inorder, preorder, inStart, inIndex - 1, preIndex);
+    node->right=buildTree(inorder, preorder, inIndex + 1, inEnd, preIndex);
 
     return node;
 }
 
 void printInorder(struct Node* node) {
-    if (node == NULL)
+    if (node==NULL)
         return;
     printInorder(node->left);
     printf("%d ", node->data);
@@ -50,7 +50,7 @@ void printInorder(struct Node* node) {
 }
 
 void printPreorder(struct Node* node) {
-    if (node == NULL)
+    if (node==NULL)
         return;
     printf("%d ", node->data);
     printPreorder(node->left);
@@ -64,15 +64,15 @@ int main() {
 
     int inorder[n], preorder[n];
     printf("Enter inorder traversal:\n");
-    for (int i = 0; i < n; i++)
+    for (int i=0;i<n;i++)
         scanf("%d", &inorder[i]);
 
     printf("Enter preorder traversal:\n");
-    for (int i = 0; i < n; i++)
+    for (int i=0;i<n;i++)
         scanf("%d", &preorder[i]);
 
     int preIndex = 0;
-    struct Node* root = buildTree(inorder, preorder, 0, n - 1, &preIndex);
+    struct Node* root=buildTree(inorder, preorder, 0, n-1, &preIndex);
 
     printf("Inorder of constructed tree: ");
     printInorder(root);
