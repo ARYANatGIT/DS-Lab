ebc5f747c3c03fc074bd1f1f3f5dd798395c690a
ARYANatGIT <anonymous.guy.26072006@gmail.com>
2025-04-25 02:43:30 +0530

    Update Q2.c

--- DS-Lab-Evaluation (III)/Q2.c
+++ DS-Lab-Evaluation (III)/Q2.c
@@ -4,22 +4,22 @@
 
 int main() {
     int num, queue[SIZE];
-    int front = 0, rear = -1;
+    int front=0,rear=-1;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num == 0) {
-        queue[++rear] = 0;
+    if (num==0) {
+        queue[++rear]=0;
     } else {
-        while (num > 0) {
-            queue[++rear] = num % 2;
-            num /= 2;
+        while (num>0) {
+            queue[++rear]=num%2;
+            num/=2;
         }
     }
 
     printf("Binary: ");
-    for (int i = rear; i >= front; i--) {
+    for (int i=rear;i>=front;i--) {
         printf("%d", queue[i]);
     }
     printf("\n");
